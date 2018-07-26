16e71dc4b055160d281a3199388e63587932f7fc
Ivan Caceres <ivanny109@gmail.com>
2018-07-26 12:16:22 -0400

    adding local development SSL support

--- geekbeacon/settings/dev.py
+++ geekbeacon/settings/dev.py
@@ -11,6 +11,7 @@ SECRET_KEY = '3apeoe(rfqxy9ik1j_zlf4%dx7$hopd2x$bd@y$4j=+yhn9j8v'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+RUNSERVERPLUS_SERVER_ADDRESS_PORT = '0.0.0.0:8000'
 
 try:
     from .local import *
